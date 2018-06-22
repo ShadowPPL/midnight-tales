e6d09ab5927b2bf6372b0a0703f5e44333fc7c61
Held Grijo <shadyueh@gmail.com>
2018-06-21 21:05:26 -0400

    Criação da captura do input e transição de animações de movimento

---
+++ Assets/Characters/Ryu/Fighter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour {
+
+	public Animator Animacao;
+	public float velx = 0.3f;
+	public float vely = 0.5f;
+
+
+	// Use this for initialization
+	void Start () {
+		Animacao = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw ("Horizontal") > 0) {
+			Animacao.SetBool ("forward", true);
+			transform.Translate(velx,0f,0f);
+		} else
+			Animacao.SetBool ("forward", false);
+
+		if (Input.GetAxisRaw ("Horizontal") < 0) {
+			Animacao.SetBool ("backward", true);
+			transform.Translate(-velx,0f,0f);
+		} else
+			Animacao.SetBool ("backward", false);
+		
+		if (Input.GetAxisRaw ("Vertical") > 0) {
+			Animacao.SetBool ("up", true);
+			transform.Translate(0f,vely,0f);
+		} else
+			Animacao.SetBool ("up", false);
+		
+		if (Input.GetAxisRaw ("Vertical") < 0) {
+			Animacao.SetBool ("down", true);
+		} else
+			Animacao.SetBool ("down", false);
+	}
+}
