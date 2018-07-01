9cb3a3274fa76cc5df3391bbb183290f625e483e
Held Grijo <shadyueh@gmail.com>
2018-07-01 19:42:34 -0400

    Tela Título - interação com o teclado

---
+++ Assets/HUD/ButtonRef.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ButtonRef : MonoBehaviour {
+
+	public GameObject selectIndicator;
+	public bool selected;
+
+	// Use this for initialization
+	void Start () {
+		selectIndicator.SetActive (false);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		selectIndicator.SetActive (selected);	
+	}
+}
---
+++ Assets/Scenes/TitleScreen.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour {
+
+	public GameObject startText;
+	float timer;
+	bool loadingLevel;
+	bool init;
+
+	public int activeElement;
+	public GameObject menuObj;
+	public ButtonRef[] menuOptions;
+
+	// Use this for initialization
+	void Start () {
+		menuObj.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!init) {
+			// it flickers start
+			timer += Time.deltaTime;
+			if (timer > 0.6f) {
+				timer = 0;
+				startText.SetActive (!startText.activeInHierarchy);
+			}
+		
+
+			if (Input.GetKeyUp (KeyCode.Space)) {
+				init = true;
+				startText.SetActive (false);
+				menuObj.SetActive (true);
+			}
+
+			if (Input.GetKeyUp (KeyCode.Escape)) {
+				init = false;
+				startText.SetActive (true);
+				menuObj.SetActive (false);
+			}
+		} else {
+			if (!loadingLevel) {
+				menuOptions [activeElement].selected = true;
+
+				if (Input.GetKeyUp (KeyCode.UpArrow)) {
+					menuOptions [activeElement].selected = false;
+					if (activeElement > 0) {
+						activeElement--;
+					} else {
+						activeElement = menuOptions.Length - 1;
+					}
+				}
+
+				if (Input.GetKeyUp (KeyCode.DownArrow)) {
+					menuOptions [activeElement].selected = false;
+					if (activeElement < menuOptions.Length - 1) {
+						activeElement++;
+					} else {
+						activeElement = 0;
+					}
+				}
+
+			}
+		}
+
+	}
+}
